5558e795035eff86c660c36ca31515190b807142
Akshay060606 <70689024+Akshay060606@users.noreply.github.com>
2024-02-26 12:10:03 +0530

    Initial commit

---
+++ app/src/main/java/launcher/axay/cmd/LauncherActivity.kt
@@ -0,0 +1,11 @@
+package launcher.axay.cmd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
---
+++ app/src/main/java/launcher/axay/cmd/MainActivity.kt
@@ -0,0 +1,56 @@
+package launcher.axay.cmd
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import launcher.axay.cmd.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding
+            .instaPromo
+            .makeClickable(
+                text = ":// For updates, follow on Instagram",
+                clickableText = "Instagram",
+                clickListener = {
+                    val uri = Uri.parse("https://www.instagram.com/axay.06")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
+            )
+    }
+
+    private fun TextView.makeClickable(text: CharSequence, clickableText: String, clickListener: () -> Unit) {
+        val spannable = SpannableString(text)
+        val clickableStart = text.indexOf(clickableText)
+        val clickableEnd = clickableStart + clickableText.length
+        spannable.setSpan(object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                clickListener.invoke()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true
+            }
+        }, clickableStart, clickableEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        this.text = spannable
+        this.movementMethod = LinkMovementMethod.getInstance()
+    }
+}
